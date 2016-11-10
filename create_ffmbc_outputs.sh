ef8e4ea8d79d65e3576c782a9a1f652657f05408
Neil Slater <neil@aframe.com>
2016-11-10 12:52:36 +0000

    Demonstrate that passing output of ffmpeg though ffmbc fixes timing issues . . .

--- create_ffmbc_outputs.sh
+++ create_ffmbc_outputs.sh
@@ -15,3 +15,13 @@ echo "$OUTPUT_CLIP duration (expected 2.40s)"
 ffprobe $OUTPUT_CLIP 2>&1 | grep "Duration:"
 
 echo ""
+
+# This requires that ffmpeg_flower_h264_aac.mp4 has already been created . . .
+OUTPUT_CLIP="outputs/ffmbc_from_ffmpeg.mp4"
+echo "Creating $OUTPUT_CLIP"
+ffmbc -y -i outputs/ffmpeg_flower_h264_aac.mp4 -f mp4 -vcodec libx264 -pix_fmt yuv420p -vb 1M -acodec libfaac $OUTPUT_CLIP  >/dev/null 2>&1
+
+echo "$OUTPUT_CLIP duration (expected 0.53s)"
+ffprobe $OUTPUT_CLIP 2>&1 | grep "Duration:"
+
+echo ""
